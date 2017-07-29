c4de3e55bbeab0f93fd1929e54892bd331c88aae
Cindy Colucy <ckcolucy@gmail.com>
2017-07-29 16:28:52 -0400

    2 passed

--- src/main/java/babysitter/Babysitter.java
+++ src/main/java/babysitter/Babysitter.java
@@ -2,18 +2,29 @@ package babysitter;
 
 public class Babysitter {
 
-	private int bedTime = 22;
+	// private int startTime = 17;
+	private int bedtime = 22;
 
 	public boolean isValidStartTime(int currentTime) {
-		if (currentTime >= 17 || currentTime <= 4) {
+		if ((currentTime >= 17 && currentTime <= 24) || (currentTime > 0 && currentTime <= 4)) {
 			return true;
 		}
 		return false;
 
 	}
 
-	public int getBedTime() {
+	public int getBedtime() {
+		return bedtime;
+	}
 
-		return bedTime;
+	public int getPayment(int startTime, int endTime) {
+		int totalPayment = 0;
+		int hourlyPayment = 12;
+		for (int currentTime = startTime; currentTime <= bedtime; currentTime++) {
+			if (currentTime < bedtime) {
+			}
+
+		}
+		return hourlyPayment;
 	}
 }
--- src/test/java/babysitter/BabysitterTest.java
+++ src/test/java/babysitter/BabysitterTest.java
@@ -2,28 +2,38 @@ package babysitter;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class BabysitterTest {
+	
+	Babysitter babysitter;
+	
+	@Before
+	public void setup() {
+		babysitter = new Babysitter();
+	}
 
 	@Test
 	public void whenStartTimeIsBeforeFivePm() {
-		Babysitter babysitter = new Babysitter();
 		int startTime = 16;
 		assertEquals(babysitter.isValidStartTime(startTime), false);
 	}
 
 	@Test
 	public void whenStartTimeIsAfterFivePm() {
-		Babysitter babysitter = new Babysitter();
 		int startTime = 18;
 		assertEquals(babysitter.isValidStartTime(startTime), true);
 	}
 
 	@Test
 	public void makeSureMyBedtimeIsTenPm() {
-		Babysitter babysitter = new Babysitter();
-		int bedTime = 22;
-		assertEquals(babysitter.getBedTime(), 22);
+		assertEquals(babysitter.getBedtime(), 22);
+	}
+
+	@Test
+	public void fromStartTimeToBedtimePaidCorrectly() {
+		//int payment = 12;
+		assertEquals(babysitter.getPayment(17, 22), 12);
 	}
 }
